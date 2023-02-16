41f4c98730fce0484124534d24eb090906a2d503
mikhita <mikheilispiriani@yahoo.com>
2023-02-16 21:19:38 +0400

    4.5*: helper functions and unit tests, step3

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -27,6 +27,14 @@ describe('total likes', () => {
       __v: 0
     },
     {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical ',
+      author: 'Edsger ',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    },
+    {
       _id: '5a422b891b54a676234d17fa',
       title: 'First class tests',
       author: 'Robert C. Martin',
@@ -53,7 +61,27 @@ describe('total likes', () => {
   ]
 
   test('when list has array of blogs, equals to the sum of likes of each one', () => {
-    const result = listHelper.totalLikes(blogs)
-    expect(result).toBe(36)
+    const result = listHelper.favoriteBlog(blogs)
+    const newObject = {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12
+    }
+    expect(result).toEqual(newObject)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      }
+    ]
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
   })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,17 +2,25 @@ const dummy = (blogs) => {
   if (Array.isArray(blogs)) {
     console.log(`The number of blogs is: ${blogs.length}`)
   } 
-
   return 1
 }
 
 
 const totalLikes = (blogList) => {
-  let sum = blogList.reduce((sum,blog)=> sum + blog.likes, 0 )
+  let sum = blogList.reduce((sum,blog)=> sum + blog.likes, 0)
   return blogList.length === 0 ? 0 : sum
+}
+
+const favoriteBlog = (blogList) => {
+  let max = blogList.reduce((prev, current)=> prev.likes >= current.likes ? prev : current)
+  let {title, author, likes} = max
+  let newObj = {title, author, likes}
+
+  return newObj
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
