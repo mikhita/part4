982a4bd6c2fb9337fe394a05f58cf9b186a53d3f
mikhita <mikheilispiriani@yahoo.com>
2023-02-27 15:52:55 +0400

    adding populate method for blogs and users with ref defining method

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,12 +1,12 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
+
+  response.json(blogs)
 })
 
 // blogsRouter.get('/:id', (request, response, next) => {
@@ -23,8 +23,9 @@ blogsRouter.get('/', (request, response) => {
 
 
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const user = await User.findById(body.userId)
   // const blog = new Blog(request.body)
   if (!body.likes) {
     return response.status(400).json({ error: 'likes are required' })
@@ -37,15 +38,22 @@ blogsRouter.post('/', (request, response, next) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: user.id
   })
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch((error) => next(error))
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  
+  response.json(savedBlog)
+
+  // blog
+  //   .save()
+  //   .then(result => {
+  //     response.status(201).json(result)
+  //   })
+  //   .catch((error) => next(error))
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
